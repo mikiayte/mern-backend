c997ea28c9c21e3fc9031e65733701f76a77dcb8
mikiayte <mikaayta@gmail.com>
2023-07-29 06:06:41 -0700

    finalllllll

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,14 @@ import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js';
 connectDB()
+const cors = require('cors');
+
+
 const app = express();
 
 const port = process.env.PORT || 5000;
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
