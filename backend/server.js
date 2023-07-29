f05c18614d844f57146e064123efa6009320c938
mikiayte <mikaayta@gmail.com>
2023-07-29 03:38:45 -0700

    trial

--- backend/server.js
+++ backend/server.js
@@ -15,20 +15,20 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser())
 app.use('/api/users', userRoutes)
- if(process.env.NODE_ENV === 'production')
- {
-    const __dirname = path.resolve();
+//  if(process.env.NODE_ENV === 'production')
+//  {
+//     const __dirname = path.resolve();
 
-    app.use(express.static(path.join(__dirname, 'frontend/dist')))
+//     app.use(express.static(path.join(__dirname, 'frontend/dist')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'index.html')));
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'index.html')));
 
- }
+//  }
 
- else {
-    app.get('/', (req, res) => res.send('server is ready'))
+//  else {
+//     app.get('/', (req, res) => res.send('server is ready'))
 
- }
+//  }
  
 app.use(notFound)
 app.use(errorHandler)
